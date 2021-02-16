2283cc974ed6d8db66968ab4e961fc1cb7ab3fe7
Saryn Mooney <smooney15@my.whitworth.edu>
2021-02-16 14:34:38 -0700

    add show action

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -4,4 +4,14 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+  private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip, :open)
+  end
+
 end
