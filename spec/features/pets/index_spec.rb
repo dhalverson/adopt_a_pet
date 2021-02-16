b3f8a4e2333cb0f84180c6ac788093f3a1ed15a9
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-16 11:26:47 -0700

    Adds pet index test

---
+++ spec/features/pets/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Pet' do
+  it 'as a visitor, I can see all exisiting pets' do
+    pets = create_list(:pet, 3)
+
+    visit('/pets')
+
+    expect(page).to have_content('All Pets')
+    pets.each do |pet|
+      expect(page).to have_content(pet.name)
+      expect(page).to have_content(pet.age)
+      expect(page).to have_content(pet.sex)
+      expect(page).to have_content(pet.type)
+      expect(page).to have_content(pet.adopted)
+      expect(page).to have_content(pet.description)
+    end
+  end
+end
