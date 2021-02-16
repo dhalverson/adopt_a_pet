eed2e9ab6d694a2bd5e478e6c70224cfcf95375b
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-15 18:58:38 -0700

    Adds shelter testing and validations

---
+++ db/migrate/20210216014237_create_shelters.rb
@@ -0,0 +1,14 @@
+class CreateShelters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shelters do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.boolean :open
+
+      t.timestamps
+    end
+  end
+end
