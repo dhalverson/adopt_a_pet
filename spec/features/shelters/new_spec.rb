39ce3742496b64f7464cddb6d74d4a2be355b46a
Saryn Mooney <smooney15@my.whitworth.edu>
2021-02-17 15:25:49 -0700

    finish new spec

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -16,5 +16,23 @@ RSpec.describe 'Shelter' do
     expect(page).to have_content("State")
     expect(page).to have_content("Zip")
     expect(page).to have_content("Open")
+
+    fill_in :name, with: "Corgi Shelter"
+    fill_in :address, with: "2468 Corgi Drive"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: "80209"
+    fill_in :open, with: true
+
+    click_on "Create Shelter"
+
+    expect(current_path).to eq('/shelters')
+    expect(Shelter.count).to eq(1)
+    expect(page).to have_content("Corgi Shelter")
+    expect(page).to have_content("2468 Corgi Drive")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("CO")
+    expect(page).to have_content("80209")
+    expect(page).to have_content(true)
   end
 end
