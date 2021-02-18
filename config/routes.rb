717a97553a0a96e4a33689018deb2dc00558c3cb
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-17 18:02:51 -0700

    Adds edit pet view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/shelters', to: 'shelters#index'
   get '/shelters/:id/pets', to: 'shelter_pets#index'
-  resources :pets, only: [:index, :show]
+  resources :pets, only: [:index, :show, :edit]
 end
