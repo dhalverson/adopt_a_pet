a797cb17823a738585c360132254c80c6063861c
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-19 13:54:00 -0700

    Adds create pet link on shelter pets page

--- spec/features/pets/create_spec.rb
+++ spec/features/pets/create_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'as a visitor' do
   before(:each) do
-    @pet_1 = create(:pet)
+    @shelter_1 = create(:shelter)
+    @pet_1 = create(:pet, shelter: @shelter_1)
   end
+  it 'I see a link to create a new pet on the shelters pets index page' do
+    visit("/shelters/#{@shelter_1.id}/pets")
+
+    expect(page).to have_link('Create Pet')
+  end
+
+  it ''
+end
  
