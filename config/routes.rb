273afc727110be1562b32ce80eba04d227202a89
Saryn Mooney <smooney15@my.whitworth.edu>
2021-02-19 20:12:13 -0700

    refactor shelter routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/shelters', to: 'shelters#index'
-  #resources :shelters
-  get '/shelters/:id/pets', to: 'shelter_pets#index'
+  resources :shelters
   resources :pets, only: [:index, :show, :edit]
-  get '/shelters/new', to: 'shelters#new'
-  post '/shelters', to: 'shelters#create'
-  get '/shelters/:id', to: 'shelters#show'
-  get '/shelters/:id/edit', to: 'shelters#edit'
-  patch '/shelters/:id', to: 'shelters#update'
-  delete '/shelters/:id', to: 'shelters#destroy'
+  get '/shelters/:id/pets', to: 'shelter_pets#index'
 end
