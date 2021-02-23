5c41eb602c50065df9ea4d122b5bc4df3db5633c
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-23 14:02:48 -0700

    Updates routes, will need to refactor still

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/shelters/:id/pets', to: 'shelter_pets#index'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
   resources :pets, :shelters
 end
