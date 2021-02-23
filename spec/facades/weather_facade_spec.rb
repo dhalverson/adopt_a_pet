0f13ba612fc6c6e2df09a9cefd51c6477225a603
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-23 13:36:22 -0700

    Adds weather facade testing

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'Weather Facade' do
       zipcode = 80120
       forecast = WeatherFacade.get_weather_forecast(zipcode)
 
+      expect(forecast).to be_a(WeatherDetails)
+      expect(forecast.temp_f).to be_a(Float)
+      expect(forecast.text).to be_a(String)
+      expect(forecast.icon).to be_a(String)
     end
   end
 end
