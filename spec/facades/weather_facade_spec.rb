ae41d7797e70788f89c2b5ce34923a2fa22ee4ba
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-02-23 13:40:52 -0700

    Adds vcr to testing

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Weather Facade' do
-  it 'returns a forecast object for a zipcode' do
-    VCR.use_cassette('weather_forecast') do
-      zipcode = 80120
-      forecast = WeatherFacade.get_weather_forecast(zipcode)
+  it 'returns a forecast object for a zipcode', :vcr do
+    zipcode = 80120
+    forecast = WeatherFacade.get_weather_forecast(zipcode)
 
-      expect(forecast).to be_a(WeatherDetails)
-      expect(forecast.temp_f).to be_a(Float)
-      expect(forecast.text).to be_a(String)
-      expect(forecast.icon).to be_a(String)
-    end
+    expect(forecast).to be_a(WeatherDetails)
+    expect(forecast.temp_f).to be_a(Float)
+    expect(forecast.text).to be_a(String)
+    expect(forecast.icon).to be_a(String)
   end
 end
